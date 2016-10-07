53763c291c1d8232c00770cfa0ad7503681dfa33
Sam Mousigian <smousigi@umich.edu>
2016-10-07 14:24:55 -0400

    added subtract functionality

--- rpn.py
+++ rpn.py
@@ -8,6 +8,11 @@ def calculate(myarg1):
 			arg2 = stack.pop()
 			result = arg1 + arg2
 			stack.append(result)
+		elif(token == "-"):
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg2 - arg1
+			stack.append(result)
 		else:
 			stack.append(int(token))		
 		print(stack)
