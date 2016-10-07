321097b5da7b8fd3669f65abf438c2186465f28a
Sam Mousigian <smousigi@umich.edu>
2016-10-07 14:35:18 -0400

    add * and / tests

--- test_rpn.py
+++ test_rpn.py
@@ -11,3 +11,9 @@ class TestBasics(unittest.TestCase):
 	def test_toomanythings(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
+	def test_mult(self):
+		result = rpn.calculate("5 5 *")
+		self.assertEqual(25, result)
+	def test_divide(self):
+		result = rpn.calculate("10 2 /")
+		self.assertEqual(5, result)
