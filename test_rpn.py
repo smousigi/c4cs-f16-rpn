def1e9bd354958aae849af38182cafae078ce15d
Sam Mousigian <smousigi@umich.edu>
2016-10-13 22:36:37 -0400

    added ^ test

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,7 @@ class TestBasics(unittest.TestCase):
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
+	def test_pow(self):
+		result = rpn.calculate("2 3 ^")
+		self.assertEqual(8, result)
+
