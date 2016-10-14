9dea822f967d3cf6fedb3a4e58e8fa445588f690
Sam Mousigian <smousigi@umich.edu>
2016-10-13 22:27:51 -0400

    changed fiels to match staff files, tried new script

--- test_rpn.py
+++ test_rpn.py
@@ -1,19 +1,20 @@
 import unittest
+
 import rpn
 
 class TestBasics(unittest.TestCase):
 	def test_add(self):
-        	result = rpn.calculate("1 1 +")
-        	self.assertEqual(2, result)
+		result = rpn.calculate("1 1 +")
+		self.assertEqual(2, result)
 	def test_subtract(self):
 		result = rpn.calculate("5 3 -")
 		self.assertEqual(2, result)
-	def test_toomanythings(self):
+	def test_multiply(self):
+		result = rpn.calculate("5 3 *")
+		self.assertEqual(15, result)
+	def test_divide(self):
+		result = rpn.calculate("6 3 /")
+		self.assertEqual(2, result)
+	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
-	def test_mult(self):
-		result = rpn.calculate("5 5 *")
-		self.assertEqual(25, result)
-	def test_divide(self):
-		result = rpn.calculate("10 2 /")
-		self.assertEqual(5, result)
